0ecf73e62cdba4468492ab348a7efeeefe2220c0
Francisco Naranjo <Naranjofjx@gmail.com>
2023-11-29 11:52:54 -0400

    Configserver pendint to work

--- microservice.course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice.course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "mscv-student" ,url = "localhost:8090/api/student")
+@FeignClient(name = "mscv-student" ,url = "localhost:8080/api/student")
 public interface StudentClient {
 
     @GetMapping("/search-by-course/{idCourse}")
